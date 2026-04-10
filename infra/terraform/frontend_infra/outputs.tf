d60230669593b8198fb13f84446717dc9bf5904a
KP <krishp4t3l@outlook.com>
2026-04-10 23:24:12 +0100

    Added hosting infrastruture

---
+++ infra/terraform/frontend_infra/outputs.tf
@@ -0,0 +1,7 @@
+output "frontend_bucket_name" {
+  value = aws_s3_bucket.frontend.bucket
+}
+
+output "frontend_cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.frontend.domain_name
+}
